1f3b31703e6ee54c58043f08f0a5d5fd36edb9cc
DeathWeasel1337 <deathweasel1337@gmail.com>
2020-01-15 16:45:09 -0600

    Map shaders to asset bundles

--- Assets/Editor/Constants.cs
+++ Assets/Editor/Constants.cs
@@ -8,7 +8,26 @@ namespace IllusionMods.KoikatuModdingTools
         internal const string ExamplesPath = @"Assets\Examples";
         internal const string SB3UtilityScriptPath = @"Tools\SB3UGS\SB3UtilityScript.exe";
 
-        internal static readonly Dictionary<string, string> ShaderABs = new Dictionary<string, string>() { { "Shader Forge/main_item", @"chara\ao_arm_00.unity3d" } };
         internal static readonly HashSet<string> GameNameList = new HashSet<string>() { "koikatsu", "koikatu", "コイカツ" };
+        internal static readonly Dictionary<string, string> ShaderABs = new Dictionary<string, string>()
+        {
+            { "Shader Forge/create_body", "chara/mm_base.unity3d" },
+            { "Shader Forge/create_eye", "chara/mm_base.unity3d" },
+            { "Shader Forge/create_hair", "chara/mm_base.unity3d" },
+            { "Shader Forge/create_head", "chara/mm_base.unity3d" },
+            { "Shader Forge/create_topN", "chara/mm_base.unity3d" },
+            { "Shader Forge/create_head", "chara/mm_base.unity3d" },
+            { "Shader Forge/main_color", "chara/ao_head_00.unity3d" },
+            { "Shader Forge/main_hair", "chara/bo_hair_b_00.unity3d" },
+            { "Shader Forge/main_hair_front", "chara/bo_hair_f_00.unity3d" },
+            { "Shader Forge/main_hair_low", "chara/bo_hair_b_00.unity3d" },
+            { "Shader Forge/main_item", "chara/ao_arm_00.unity3d" },
+            { "Shader Forge/main_item_ditherd", "chara/ao_head_09.unity3d" },
+            { "Shader Forge/main_item_emission", "chara/ao_head_08.unity3d" },
+            { "Shader Forge/main_item_low", "chara/bo_hair_b_00.unity3d" },
+            { "Shader Forge/main_item_studio_add", "chara/ao_head_08.unity3d" },
+            { "Shader Forge/main_texture", "chara/ao_hair_00.unity3d" },
+            { "Shader Forge/toon_glasses_lod0", "chara/ao_face_00.unity3d" },
+        };
     }
 }
