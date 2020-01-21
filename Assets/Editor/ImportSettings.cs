9656f68a4a7d4f5862a51f4dff4a914811003e3e
DeathWeasel1337 <deathweasel1337@gmail.com>
2020-01-21 14:29:54 -0600

    Disable ImportSettings

--- Assets/Editor/ImportSettings.cs
+++ Assets/Editor/ImportSettings.cs
@@ -1,15 +1,15 @@
-﻿using UnityEditor;
-namespace IllusionMods.KoikatuModdingTools
-{
-    /// <summary>
-    /// Automatically sets texture compression for newly added textures to High Quality (BC7)
-    /// </summary>
-    internal class TexturePostprocessor : AssetPostprocessor
-    {
-        internal void OnPreprocessTexture()
-        {
-            TextureImporter textureImporter = (TextureImporter)assetImporter;
-            textureImporter.textureCompression = TextureImporterCompression.CompressedHQ;
-        }
-    }
-}
+﻿//using UnityEditor;
+//namespace IllusionMods.KoikatuModdingTools
+//{
+//    /// <summary>
+//    /// Automatically sets texture compression for newly added textures to High Quality (BC7)
+//    /// </summary>
+//    internal class TexturePostprocessor : AssetPostprocessor
+//    {
+//        internal void OnPreprocessTexture()
+//        {
+//            TextureImporter textureImporter = (TextureImporter)assetImporter;
+//            textureImporter.textureCompression = TextureImporterCompression.CompressedHQ;
+//        }
+//    }
+//}
