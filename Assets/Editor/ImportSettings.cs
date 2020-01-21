6bc90d5b466b07b8bf83534827a099ab0c4abec7
DeathWeasel1337 <deathweasel1337@gmail.com>
2020-01-20 21:00:58 -0600

    Add ImportSettings

---
+++ Assets/Editor/ImportSettings.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+namespace IllusionMods.KoikatuModdingTools
+{
+    /// <summary>
+    /// Automatically sets texture compression for newly added textures to High Quality (BC7)
+    /// </summary>
+    internal class TexturePostprocessor : AssetPostprocessor
+    {
+        internal void OnPreprocessTexture()
+        {
+            TextureImporter textureImporter = (TextureImporter)assetImporter;
+            textureImporter.textureCompression = TextureImporterCompression.CompressedHQ;
+        }
+    }
+}
