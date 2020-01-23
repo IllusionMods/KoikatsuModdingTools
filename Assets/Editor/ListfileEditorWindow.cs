b78b778320bf5191e19884357d6f120e99f5f862
DeathWeasel1337 <deathweasel1337@gmail.com>
2020-01-23 10:17:30 -0600

    Move ListfileEditorWindow

--- Assets/Editor/ListfileEditorWindow.cs
+++ Assets/Editor/ListfileEditorWindow.cs
@@ -6,7 +6,7 @@ using System.Text;
 using UnityEditor;
 using UnityEngine;
 
-namespace Editor
+namespace IllusionMods.KoikatuModdingTools
 {
     public class ListfileEditorWindow : EditorWindow
     {
