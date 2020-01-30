8b9493b0cb642a0a7fb435c135f9c8593b91707a
DeathWeasel1337 <deathweasel1337@gmail.com>
2020-01-29 18:14:42 -0600

    Cleanup

--- Assets/Scripts/Map/SunLightInfo.cs
+++ Assets/Scripts/Map/SunLightInfo.cs
@@ -7,37 +7,33 @@ public class SunLightInfo : MonoBehaviour
 {
     [SerializeField]
     private Light _targetLight;
-
     [SerializeField]
     private BackGroundParam _bgParam;
-
     [SerializeField]
     private SunLightInfo.Info[] _infos;
+
+    public enum Type
+    {
+        DayTime = 0,
+        Evening = 1,
+        Night = 2,
+    }
 
     [Serializable]
     public class Info
     {
-        public int type;
-
+        public Type type;
         public Texture2D backTexture;
-
         public List<GameObject> visibleList = new List<GameObject>();
-
         public Vector3 angle;
-
         public Color color = Color.white;
-
         public float intensity = 1f;
-
         public bool fogUse;
-
         public Color fogColor = Color.white;
         public float fogStart = 0f;
         public float fogEnd = 80f;
-
         public Color sunShaftsColor;
         public Transform sunShaftsTransform;
-
         public Texture aceLutTexture;
         public Texture aceLutBlendTexture;
         public float aceBlendAmount;
