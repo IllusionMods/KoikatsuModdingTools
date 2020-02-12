37eab010640a9a8b144b0fbe118fe119fd3ba850
DeathWeasel1337 <deathweasel1337@gmail.com>
2020-02-12 12:33:10 -0600

    Flesh out list file editor

--- Assets/Editor/ListfileEditorWindow.cs
+++ Assets/Editor/ListfileEditorWindow.cs
@@ -1,142 +1,90 @@
-﻿using System;
+﻿using IllusionMods.KoikatuModdingTools.Lists;
+using System;
 using System.Collections.Generic;
-using System.Globalization;
+using System.IO;
 using System.Linq;
-using System.Text;
 using UnityEditor;
 using UnityEngine;
 
 namespace IllusionMods.KoikatuModdingTools
 {
-    public class ListfileEditorWindow : EditorWindow
+    [CustomEditor(typeof(TextAsset))]
+    public class CSVEditor : Editor
     {
-        public enum CategoryNo
+        public static MakerListFile makerListFile;
+        public static CSVEditor instance;
+
+        internal void OnEnable()
         {
-            cha_sample_f,
-            cha_sample_m,
-            cha_eyeset,
-            bodypaint_layout,
-            cha_default_cos_f,
-            cha_default_cos_m,
-            cha_sample_voice,
-            facepaint_layout,
-            mole_layout,
-            cha_sample_m_ex,
-            bo_head = 100,
-            bo_hair_b,
-            bo_hair_f,
-            bo_hair_s,
-            bo_hair_o,
-            co_top,
-            co_bot,
-            co_bra,
-            co_shorts,
-            co_gloves,
-            co_panst,
-            co_socks,
-            co_shoes,
-            ao_none = 120,
-            ao_hair,
-            ao_head,
-            ao_face,
-            ao_neck,
-            ao_body,
-            ao_waist,
-            ao_leg,
-            ao_arm,
-            ao_hand,
-            ao_kokan,
-            cpo_sailor_a = 200,
-            cpo_sailor_b,
-            cpo_sailor_c,
-            cpo_jacket_a = 210,
-            cpo_jacket_b,
-            cpo_jacket_c,
-            mt_face_detail = 400,
-            mt_eyeshadow,
-            mt_cheek,
-            mt_lip,
-            mt_lipline,
-            mt_face_paint,
-            mt_eyebrow,
-            mt_eye_white,
-            mt_eye,
-            mt_eye_gradation,
-            mt_eye_hi_up,
-            mt_eye_hi_down,
-            mt_eyeline_up,
-            mt_eyeline_down,
-            mt_nose,
-            mt_mole,
-            face_preset,
-            mt_body_detail = 420,
-            mt_body_paint,
-            mt_sunburn,
-            mt_nip,
-            mt_underhair,
-            mt_pattern = 430,
-            mt_emblem,
-            mt_ramp,
-            mt_hairgloss,
-            ex_bo_body = 500,
-            ex_bo_head,
-            ex_bo_hair,
-            ex_co_clothes,
-            ex_co_shoes
+            instance = this;
+            string path = AssetDatabase.GetAssetPath(target);
+            if (path.ToLower().Contains("list/maker/") && path.ToLower().EndsWith(".csv"))
+                makerListFile = new MakerListFile(new FileInfo(path));
+            else
+                makerListFile = null;
         }
 
-        private static readonly Dictionary<CategoryNo, CategoryDefinition> categoryColumnDefinitions = new Dictionary<CategoryNo, CategoryDefinition>
+        public override void OnInspectorGUI()
         {
-            {CategoryNo.cha_sample_f, new CategoryDefinition(new[] {"0", "0"}, new[] {new ColumnInfo("IntTest", IntSanitizer), new ColumnInfo("StringTest", StringSanitizer)})},
-            {CategoryNo.cha_sample_m, new CategoryDefinition(new[] {"0"}, new[] {new ColumnInfo("IntTest2", IntSanitizer), new ColumnInfo("StringTest2", StringSanitizer) })}
-        };
+            if (makerListFile != null)
+            {
+                CSVInspectorGUI();
+                base.OnInspectorGUI();
+            }
+            else
+            {
+                base.OnInspectorGUI();
+            }
+        }
 
+        private void CSVInspectorGUI()
+        {
+            GUI.enabled = true;
+
+            if (GUILayout.Button("Open List File Editor"))
+            {
+                ListfileEditorWindow window = (ListfileEditorWindow)EditorWindow.GetWindow(typeof(ListfileEditorWindow), false, "List File Editor");
+                window.Show();
+            }
+
+            GUILayout.Label(makerListFile.CSVFileInfo.Name);
+            GUI.enabled = false;
+            GUILayout.TextArea(makerListFile.GetCSVText(), GUILayout.ExpandHeight(true), GUILayout.ExpandWidth(true), GUILayout.MinHeight(70), GUILayout.MaxHeight(250));
+            GUI.enabled = true;
+        }
+    }
+
+    public class ListfileEditorWindow : EditorWindow
+    {
         private static readonly string[] categoryListLookup = Enum.GetValues(typeof(CategoryNo)).Cast<CategoryNo>().Select(x => (int)x + " - " + x.ToString()).ToArray();
         private static readonly List<CategoryNo> categoryListLookup2 = Enum.GetValues(typeof(CategoryNo)).Cast<CategoryNo>().ToList();
 
         [SerializeField]
-        private int categoryListPosition = -1;
+        private static int categoryListPosition = -1;
         [SerializeField]
-        private Vector2 scrollPosition;
-        [SerializeField]
-        private CategoryNo currentCategory = CategoryNo.cha_sample_m;
-        [SerializeField]
-        private CategoryDefinition currentDefinition;
-        [SerializeField]
-        private List<Row> currentRows = new List<Row>();
-        [SerializeField]
-        private List<string> currentMagicRows = new List<string>();
+        private static Vector2 scrollPosition;
 
-        [MenuItem("Window/ListFile Editor")]
-        public static void ShowWindow()
+        internal void OnEnable()
         {
-            GetWindow(typeof(ListfileEditorWindow));
+            categoryListPosition = categoryListLookup2.IndexOf(CSVEditor.makerListFile.Category);
         }
 
-        // Use this for initialization
-        private void Awake()
+        internal void OnGUI()
         {
-            titleContent = new GUIContent("ListFile Editor");
-            SetCategory(currentCategory);
-        }
-
-        private void OnGUI()
-        {
-            //GUILayout.BeginHorizontal(EditorStyles.toolbar);
-
-            //todo a way to chose a file
-
             GUILayout.BeginHorizontal(GUI.skin.box);
             {
                 GUILayout.Label("List file category", GUILayout.ExpandWidth(false));
                 GUI.changed = false;
                 categoryListPosition = EditorGUILayout.Popup(categoryListPosition, categoryListLookup);
                 if (GUI.changed)
-                    SetCategory(categoryListLookup2[categoryListPosition]);
+                {
+                    CSVEditor.makerListFile.Category = categoryListLookup2[categoryListPosition];
+                    CSVEditor.makerListFile.Save();
+                }
             }
             GUILayout.EndHorizontal();
 
-            if (currentDefinition == null)
+            if (CSVEditor.makerListFile.CategoryKeys == null)
             {
                 GUILayout.Label("The selected listfile type is not supported");
                 return;
@@ -145,7 +93,7 @@ namespace IllusionMods.KoikatuModdingTools
             var totalW = position.width;
             var hashW = 12;
             var spaceW = 20;
-            var columnW = (int)((totalW - hashW) / currentDefinition.Columns.Length) - spaceW;
+            var columnW = (int)((totalW - hashW) / CSVEditor.makerListFile.CategoryKeys.Length) - spaceW;
 
             GUILayout.BeginVertical(GUI.skin.box);
             {
@@ -153,8 +101,8 @@ namespace IllusionMods.KoikatuModdingTools
                 GUILayout.BeginHorizontal();
                 {
                     GUILayout.Label("#", GUILayout.Width(hashW));
-                    foreach (var column in currentDefinition.Columns)
-                        GUILayout.Label(column.Name, GUILayout.Width(columnW));
+                    foreach (var column in CSVEditor.makerListFile.CategoryKeys)
+                        GUILayout.Label(column.ToString(), GUILayout.Width(columnW));
                 }
                 GUILayout.EndHorizontal();
             }
@@ -162,22 +110,25 @@ namespace IllusionMods.KoikatuModdingTools
 
             scrollPosition = GUILayout.BeginScrollView(scrollPosition, false, true);
             {
-                for (var rowIndex = 0; rowIndex < currentRows.Count; rowIndex++)
+                for (var rowIndex = 0; rowIndex < CSVEditor.makerListFile.Data.Count; rowIndex++)
                 {
-                    var row = currentRows[rowIndex];
+                    var row = CSVEditor.makerListFile.Data[rowIndex];
                     GUILayout.BeginHorizontal(GUI.skin.box);
                     {
-                        GUILayout.Label(rowIndex.ToString(), GUILayout.Width(hashW));
-                        for (var rowContentIndex = 0; rowContentIndex < row.Contents.Count; rowContentIndex++)
+                        GUILayout.Label((rowIndex + 1).ToString(), GUILayout.Width(hashW));
+
+                        foreach (var key in CSVEditor.makerListFile.CategoryKeys)
                         {
                             GUI.changed = false;
-                            var value = GUILayout.TextField(row.Contents[rowContentIndex], GUILayout.Width(columnW));
+                            string data;
+                            if (!row.TryGetValue(key, out data))
+                                data = "";
+                            var value = GUILayout.TextField(data, GUILayout.Width(columnW));
                             if (GUI.changed)
                             {
-                                var valueSanitizer = currentDefinition.Columns[rowContentIndex].ValueSanitizer;
-                                if (valueSanitizer != null)
-                                    value = valueSanitizer(value);
-                                row.Contents[rowContentIndex] = value;
+                                row[key] = value;
+                                CSVEditor.instance.Repaint();
+                                CSVEditor.makerListFile.Save();
                             }
                         }
                     }
@@ -188,156 +139,22 @@ namespace IllusionMods.KoikatuModdingTools
                 {
                     if (GUILayout.Button("Add row"))
                     {
-                        var newRow = new Row();
-                        SanitizeRow(newRow);
-                        currentRows.Add(newRow);
+                        CSVEditor.makerListFile.AddDataRow();
+                        CSVEditor.instance.Repaint();
                     }
 
-                    GUI.enabled = currentRows.Count > 0;
+                    GUI.enabled = CSVEditor.makerListFile.Data.Count > 0;
                     if (GUILayout.Button("Remove last row"))
-                        currentRows.RemoveAt(currentRows.Count - 1);
+                    {
+                        CSVEditor.makerListFile.Data.RemoveAt(CSVEditor.makerListFile.Data.Count - 1);
+                        CSVEditor.instance.Repaint();
+                        CSVEditor.makerListFile.Save();
+                    }
                     GUI.enabled = true;
                 }
                 GUILayout.EndHorizontal();
             }
             GUILayout.EndScrollView();
-
-            GUILayout.Label(".csv contents:");
-            GUI.changed = false;
-            var csv = ToCsv();
-            csv = GUILayout.TextArea(csv, GUILayout.ExpandHeight(true), GUILayout.ExpandWidth(true), GUILayout.MinHeight(70), GUILayout.MaxHeight(250));
-            if (GUI.changed)
-                FromCsv(csv);
-        }
-
-        private string ToCsv()
-        {
-            var sb = new StringBuilder();
-            sb.AppendLine(((int)currentCategory).ToString(CultureInfo.InvariantCulture));
-
-            while (currentMagicRows.Count > currentDefinition.MagicRows.Length) currentMagicRows.RemoveAt(currentMagicRows.Count - 1);
-            while (currentMagicRows.Count < currentDefinition.MagicRows.Length) currentMagicRows.Add(currentDefinition.MagicRows[currentMagicRows.Count]);
-            foreach (var magicRow in currentMagicRows)
-                sb.AppendLine(magicRow);
-
-            sb.AppendLine(string.Join(",", currentDefinition.Columns.Select(x => x.Name).ToArray()));
-
-            foreach (var row in currentRows)
-                sb.AppendLine(string.Join(",", row.Contents.ToArray()));
-
-            return sb.ToString().Replace("\r\n", "\n");
-        }
-
-        private void FromCsv(string csv)
-        {
-            CategoryNo cat = 0;
-            var magicRows = new List<string>();
-            var rows = new List<Row>();
-
-            var lines = csv.Trim().Split();
-            Debug.LogWarning(lines.Length);
-            for (var index = 0; index < lines.Length; index++)
-            {
-                var line = lines[index];
-
-                if (index == 0)
-                    cat = (CategoryNo)int.Parse(line);
-                else if (index <= currentDefinition.MagicRows.Length)
-                    magicRows.Add(line);
-                // Category names
-                else if (index == currentDefinition.MagicRows.Length + 1)
-                    continue;
-                else
-                {
-                    if (line.Trim().Length == 0) continue;
-
-                    var row = new Row();
-                    row.Contents.AddRange(line.Split(','));
-                    SanitizeRow(row);
-                    rows.Add(row);
-                }
-            }
-
-            if (currentCategory != cat) SetCategory(cat);
-            currentMagicRows = magicRows;
-            currentRows = rows;
-        }
-
-        private void SetCategory(CategoryNo categoryNo)
-        {
-            currentCategory = categoryNo;
-            categoryListPosition = categoryListLookup2.IndexOf(categoryNo);
-
-            CategoryDefinition categoryDefinition;
-            categoryColumnDefinitions.TryGetValue(categoryNo, out categoryDefinition);
-            currentDefinition = categoryDefinition;
-
-            if (currentDefinition != null)
-            {
-                foreach (var row in currentRows)
-                    SanitizeRow(row);
-
-                currentMagicRows = currentDefinition.MagicRows.ToList();
-            }
-            else
-            {
-                currentRows.Clear();
-                currentMagicRows.Clear();
-            }
-        }
-
-        private void SanitizeRow(Row row)
-        {
-            while (row.Contents.Count > currentDefinition.Columns.Length) row.Contents.RemoveAt(row.Contents.Count - 1);
-            while (row.Contents.Count < currentDefinition.Columns.Length) row.Contents.Add(string.Empty);
-            for (var index = 0; index < row.Contents.Count; index++)
-            {
-                var valueSanitizer = currentDefinition.Columns[index].ValueSanitizer;
-                if (valueSanitizer != null)
-                    row.Contents[index] = valueSanitizer(row.Contents[index]);
-            }
-        }
-
-        private static string IntSanitizer(string value)
-        {
-            int result;
-            int.TryParse(value, out result);
-            return result.ToString(CultureInfo.InvariantCulture);
-        }
-        private static string StringSanitizer(string value)
-        {
-            return value != null ? value.Trim() : string.Empty;
-        }
-
-        public sealed class CategoryDefinition
-        {
-            public string[] MagicRows;
-            public ColumnInfo[] Columns;
-
-            public CategoryDefinition(string[] magicRows, ColumnInfo[] columns)
-            {
-                MagicRows = magicRows;
-                Columns = columns;
-            }
-        }
-
-        public sealed class ColumnInfo
-        {
-            public string Name;
-            public Func<string, string> ValueSanitizer;
-
-            public ColumnInfo(string name, Func<string, string> valueSanitizer)
-            {
-                Name = name;
-                ValueSanitizer = valueSanitizer;
-            }
-        }
-
-        [Serializable]
-        public sealed class Row
-        {
-            [SerializeField]
-            public List<string> Contents = new List<string>();
         }
     }
 }
---
+++ Assets/Editor/Lists/MakerListFile.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
+
+namespace IllusionMods.KoikatuModdingTools.Lists
+{
+    public class MakerListFile
+    {
+        public CategoryNo Category;
+        public List<Dictionary<KeyType, string>> Data = new List<Dictionary<KeyType, string>>();
+        public FileInfo CSVFileInfo;
+
+        public MakerListFile(FileInfo file)
+        {
+            CSVFileInfo = file;
+            List<KeyType> keys = new List<KeyType>();
+
+            using (FileStream fs = file.OpenRead())
+            {
+                using (StreamReader sr = new StreamReader(fs))
+                {
+                    int counter = 0;
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+
+                        if (counter == 0) //Category line
+                        {
+                            Category = (CategoryNo)int.Parse(line);
+                        }
+                        else if (counter == 1 || counter == 2) { }//Pointless lines
+                        else if (counter == 3) //Header line
+                        {
+                            foreach (string key in line.Trim().Split(','))
+                                keys.Add((KeyType)Enum.Parse(typeof(KeyType), key));
+                        }
+                        else //Data lines
+                        {
+                            if (string.IsNullOrEmpty(line)) continue;
+
+                            Dictionary<KeyType, string> lineData = new Dictionary<KeyType, string>();
+
+                            var lineSplit = line.Trim().Split(',');
+                            for (int i = 0; i < lineSplit.Length; i++)
+                                lineData[keys[i]] = lineSplit[i];
+
+                            Data.Add(lineData);
+                        }
+                        counter++;
+                    }
+                }
+            }
+        }
+
+        public void Save()
+        {
+            using (StreamWriter writer = new StreamWriter(CSVFileInfo.FullName))
+                writer.Write(GetCSVText());
+        }
+
+        public string GetCSVText()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine(((int)Category).ToString(CultureInfo.InvariantCulture));
+            sb.AppendLine("0");
+            sb.AppendLine("0");
+
+            List<string> keys = new List<string>();
+            foreach (var key in CategoryKeys)
+                keys.Add(key.ToString());
+            sb.AppendLine(string.Join(",", keys.ToArray()));
+
+            foreach (var line in Data)
+            {
+                List<string> lineData = new List<string>();
+                foreach (var category in CategoryKeys)
+                {
+                    string columnData;
+                    if (line.TryGetValue(category, out columnData))
+                        lineData.Add(Sanitize(category, columnData));
+                    else
+                        lineData.Add(Sanitize(category, ""));
+                }
+                sb.AppendLine(string.Join(",", lineData.ToArray()));
+            }
+
+            return sb.ToString().Replace("\r\n", "\n");
+        }
+
+        public void AddDataRow()
+        {
+            var newRow = new Dictionary<KeyType, string>();
+
+            foreach (var key in CategoryKeys)
+            {
+                DataType dataType;
+                if (KeyTypes.TryGetValue(key, out dataType))
+                    newRow[key] = Sanitize(dataType, "");
+            }
+            Data.Add(newRow);
+        }
+
+        public string GetNextID()
+        {
+            List<int> ids = new List<int> { 0 };
+
+            foreach (var x in Data)
+            {
+                var idString = x.Where(y => y.Key == KeyType.ID).Select(y => y.Value).FirstOrDefault();
+                int id;
+                if (int.TryParse(idString, out id))
+                    ids.Add(id);
+
+            }
+
+            return (ids.Max() + 1).ToString();
+        }
+
+        private string Sanitize(KeyType keyType, string input)
+        {
+            DataType dataType;
+            if (KeyTypes.TryGetValue(keyType, out dataType))
+                return Sanitize(dataType, input);
+            else
+                return Sanitize(DataType.String, input);
+        }
+
+        private string Sanitize(DataType dataType, string input)
+        {
+            if (dataType == DataType.Int)
+                return IntSanitizer(input);
+            else if (dataType == DataType.ID)
+                return IDSanitizer(input);
+            else if (dataType == DataType.Float)
+                return FloatSanitizer(input);
+            else if (dataType == DataType.Manifest)
+                return "";
+            else
+                return StringSanitizer(input);
+        }
+
+        /// <summary>
+        /// Get all the keys for the current category
+        /// </summary>
+        public KeyType[] CategoryKeys
+        {
+            get
+            {
+                KeyType[] categoryDefinition;
+                CategoryColumnDefinitions.TryGetValue(Category, out categoryDefinition);
+                return categoryDefinition;
+            }
+        }
+
+        private string IDSanitizer(string value)
+        {
+            int result;
+            if (int.TryParse(value, out result))
+                return result.ToString(CultureInfo.InvariantCulture);
+            else
+                return GetNextID();
+
+        }
+
+        private string IntSanitizer(string value)
+        {
+            int result;
+            int.TryParse(value, out result);
+            return result.ToString(CultureInfo.InvariantCulture);
+        }
+
+        private string FloatSanitizer(string value)
+        {
+            float result;
+            float.TryParse(value, out result);
+            return result.ToString(CultureInfo.InvariantCulture);
+        }
+
+        private string StringSanitizer(string value)
+        {
+            return value != null ? value.Trim() : string.Empty;
+        }
+
+        private static readonly Dictionary<CategoryNo, KeyType[]> CategoryColumnDefinitions = new Dictionary<CategoryNo, KeyType[]>
+        {
+            {CategoryNo.bodypaint_layout, new[] {KeyType.ID, KeyType.Kind, KeyType.Name, KeyType.PosX, KeyType.PosY, KeyType.Rot, KeyType.Scale, KeyType.CenterX, KeyType.MoveX, KeyType.CenterY, KeyType.MoveY, KeyType.CenterScale, KeyType.AddScale, KeyType.ThumbAB, KeyType.ThumbTex}},
+            {CategoryNo.facepaint_layout, new[] {KeyType.ID, KeyType.Kind, KeyType.Name, KeyType.PosX, KeyType.PosY, KeyType.Rot, KeyType.Scale, KeyType.ThumbAB, KeyType.ThumbTex}},
+            {CategoryNo.mole_layout, new[] {KeyType.ID, KeyType.Kind, KeyType.Name, KeyType.PosX, KeyType.PosY, KeyType.Rot, KeyType.Scale, KeyType.ThumbAB, KeyType.ThumbTex}},
+            {CategoryNo.bo_head, new[] {KeyType.ID, KeyType.Kind, KeyType.Possess, KeyType.Name, KeyType.MainManifest, KeyType.MainAB, KeyType.MainData, KeyType.ShapeAnime, KeyType.MatManifest, KeyType.MatData, KeyType.MainTexAB, KeyType.MainTex, KeyType.ColorMaskAB, KeyType.ColorMaskTex, KeyType.ThumbAB, KeyType.ThumbTex}},
+            {CategoryNo.mt_emblem, new[] {KeyType.ID, KeyType.Kind, KeyType.Possess, KeyType.Name, KeyType.MainTexAB, KeyType.MainTex, KeyType.ThumbAB, KeyType.ThumbTex}},
+        };
+
+        private static readonly Dictionary<KeyType, DataType> KeyTypes = new Dictionary<KeyType, DataType>
+        {
+            {KeyType.AddScale, DataType.Float },
+            {KeyType.CenterScale, DataType.Float },
+            {KeyType.CenterX, DataType.Float },
+            {KeyType.CenterY, DataType.Float },
+            {KeyType.ID, DataType.ID },
+            {KeyType.ColorMaskAB, DataType.AssetBundle },
+            {KeyType.ColorMaskTex, DataType.TextureAsset },
+            {KeyType.Kind, DataType.Int },
+            {KeyType.MainAB, DataType.AssetBundle },
+            {KeyType.MainData, DataType.PrefabAsset },
+            {KeyType.MainManifest, DataType.Manifest },
+            {KeyType.MainTex, DataType.TextureAsset },
+            {KeyType.MainTexAB, DataType.AssetBundle },
+            {KeyType.MatData, DataType.MaterialAsset },
+            {KeyType.MatManifest, DataType.Manifest },
+            {KeyType.MoveX, DataType.Float },
+            {KeyType.MoveY, DataType.Float },
+            {KeyType.Name, DataType.String },
+            {KeyType.Possess, DataType.Int },
+            {KeyType.PosX, DataType.Float },
+            {KeyType.PosY, DataType.Float },
+            {KeyType.Rot, DataType.Float },
+            {KeyType.ShapeAnime, DataType.Asset },
+            {KeyType.Scale, DataType.Float },
+            {KeyType.ThumbAB, DataType.AssetBundle },
+            {KeyType.ThumbTex, DataType.TextureAsset },
+        };
+
+        private enum DataType
+        {
+            ID,
+            Int,
+            String,
+            Float,
+            AssetBundle,
+            Asset,
+            PrefabAsset,
+            TextureAsset,
+            MaterialAsset,
+            Manifest,
+        }
+    }
+}
