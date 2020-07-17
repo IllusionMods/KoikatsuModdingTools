cf4e885d96f77ea34a15b5661ff9bb26f2e7777e
DeathWeasel1337 <deathweasel1337@gmail.com>
2020-07-17 02:20:43 -0500

    Convert normal maps to red style instead of grey style

---
+++ Assets/Editor/TextureProcessor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+/// <summary>
+/// Convert grey-style normalmaps to red-style to make normalmaps compatible with both KK and EC
+/// </summary>
+public class ConvertNormalMaps : AssetPostprocessor
+{
+    internal void OnPostprocessTexture(Texture2D texture)
+    {
+        var textureImpoter = assetImporter as TextureImporter;
+        if (textureImpoter.textureType != TextureImporterType.NormalMap) return;
+
+        //Set the entire red color channel to white
+        Color[] c = texture.GetPixels(0);
+        for (int i = 0; i < c.Length; i++)
+            c[i].r = 1;
+
+        texture.SetPixels(c, 0);
+        texture.Apply(true);
+    }
+}
