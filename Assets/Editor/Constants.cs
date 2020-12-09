37c1cde03ac82b690494c69f42606a66d22ee8d8
DeathWeasel1337 <deathweasel1337@gmail.com>
2020-12-09 16:15:32 -0600

    Add commented out ShaderABs, used for rebuilding the ShaderAB itself

--- Assets/Editor/Constants.cs
+++ Assets/Editor/Constants.cs
@@ -57,6 +57,54 @@ namespace IllusionMods.KoikatuModdingTools
             { "Shader Forge/toon_nose_lod0", ShaderABPath },
             { "Shader Forge/toon_textureanimation", ShaderABPath },
         };
+
+        //Note: This will pull the shaders from the actual game files rather than the included shader AB, for the purpose of building the shader AB itself.
+        //map.unity3d is from VKatsu at v_katsu\abdata\vtuber\map.unity3d. Copy it to the KK abdata folder before building.
+
+        //internal static readonly Dictionary<string, string> ShaderABs = new Dictionary<string, string>
+        //{
+        //    { "Custom/CustomUnlit-Outline",  "map.unity3d" },
+        //    { "Custom/Blend/Zero_Subtractive",  "map.unity3d" },
+        //    { "Custom/Blend/Additive",  "map.unity3d" },
+        //    { "Custom/Silhouette", "chara/oo_base.unity3d" },
+        //    { "CustomUnlit/Transparent DoubleSide",  "map.unity3d" },
+        //    { "Shader Forge/create_body", "chara/mm_base.unity3d" },
+        //    { "Shader Forge/create_eye", "chara/mm_base.unity3d" },
+        //    { "Shader Forge/create_eyewhite", "chara/mm_base.unity3d" },
+        //    { "Shader Forge/create_hair", "chara/mm_base.unity3d" },
+        //    { "Shader Forge/create_head", "chara/mm_base.unity3d" },
+        //    { "Shader Forge/create_topN", "chara/mm_base.unity3d" },
+        //    { "Shader Forge/main_alpha", "chara/co_bra_00.unity3d" },
+        //    { "Shader Forge/main_color", "chara/ao_head_00.unity3d" },
+        //    { "Shader Forge/main_emblem", "chara/co_top_00.unity3d" },
+        //    { "Shader Forge/main_emblem_clothes", "chara/co_bra_00.unity3d" },
+        //    { "Shader Forge/main_hair", "chara/bo_hair_b_00.unity3d" },
+        //    { "Shader Forge/main_hair_front", "chara/bo_hair_f_00.unity3d" },
+        //    { "Shader Forge/main_hair_low", "chara/bo_hair_b_00.unity3d" },
+        //    { "Shader Forge/main_item", "chara/ao_arm_00.unity3d" },
+        //    { "Shader Forge/main_item_ditherd", "chara/ao_head_09.unity3d" },
+        //    { "Shader Forge/main_item_emission", "chara/ao_head_08.unity3d" },
+        //    { "Shader Forge/main_item_low", "chara/bo_hair_b_00.unity3d" },
+        //    { "Shader Forge/main_item_studio", "studio/mat/00.unity3d" },
+        //    { "Shader Forge/main_item_studio_add", "studio/mat/00.unity3d" },
+        //    { "Shader Forge/main_item_studio_alpha", "studio/mat/00.unity3d" },
+        //    { "Shader Forge/main_opaque", "chara/co_bra_00.unity3d" },
+        //    { "Shader Forge/main_opaque2", "chara/co_bra_10.unity3d" },
+        //    { "Shader Forge/main_skin", "chara/mm_base.unity3d" },
+        //    { "Shader Forge/main_skin_low", "chara/mm_base.unity3d" },
+        //    { "Shader Forge/main_StandardMDK_studio", "studio/mat/00.unity3d" },
+        //    { "Shader Forge/main_opaque_low", "chara/co_bra_00.unity3d" },
+        //    { "Shader Forge/main_opaque_low2", "chara/co_bra_10.unity3d" },
+        //    { "Shader Forge/main_texture", "chara/ao_hair_00.unity3d" },
+        //    { "Shader Forge/main_texture_studio", "studio/mat/00.unity3d" },
+        //    { "Shader Forge/mnpb", "chara/oo_base.unity3d" },
+        //    { "Shader Forge/shadowcast", "chara/oo_base.unity3d" },
+        //    { "Shader Forge/toon_eye_lod0", "chara/bo_head_00.unity3d" },
+        //    { "Shader Forge/toon_eyew_lod0", "chara/bo_head_00.unity3d" },
+        //    { "Shader Forge/toon_glasses_lod0", "chara/bo_head_00.unity3d" },
+        //    { "Shader Forge/toon_nose_lod0", "chara/bo_head_00.unity3d" },
+        //    { "Shader Forge/toon_textureanimation", "chara/mm_base.unity3d" },
+        //};
     }
 
     public enum CategoryNo
