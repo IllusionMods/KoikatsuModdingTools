1669cdc583d7285b092143c1e889f334cb2b7c51
Tobias Carothers <105331328+Wogrim@users.noreply.github.com>
2022-05-16 16:16:33 -0700

    change Clothes MB to original format
    this is the old version of the clothes MB, which has been in the pins on the discord #mod-modelling for the past 6 months
    I had created it at the time because the "new" clothes MB (to my understanding, from Darkness DLC) wasn't working with optional clothes parts for me (has no useOpt fields)
    (Steam game version + Steam DLC + HF Patch)
    So for maximum user compatibility, the old version is best
    If anyone was using the new clothes MB, this would "break" their mod (if they update KKMT and rebuild it), so perhaps a Darkness branch should be created (I don't think you can have both versions in the same unity project without renaming one, which I assume would be a problem)

--- Assets/Scripts/ChaClothesComponent.cs
+++ Assets/Scripts/ChaClothesComponent.cs
@@ -8,8 +8,6 @@ public class ChaClothesComponent : MonoBehaviour
     public Renderer[] rendNormal01;
     [Tooltip("Renderers affected by the second set of MainTex and ColorMask textures configured in the list file")]
     public Renderer[] rendNormal02;
-    [Tooltip("Renderers affected by the third set of MainTex and ColorMask textures configured in the list file")]
-    public Renderer[] rendNormal03;
 
     [Tooltip("Whether to show the first color picker")]
     public bool useColorN01;
@@ -36,14 +34,10 @@ public class ChaClothesComponent : MonoBehaviour
     [Header("Emblem Parts")]
     [Tooltip("Renderer which uses the first emblem")]
     public Renderer rendEmblem01;
-    [Tooltip("Additional renderers which use the first emblem")]
-    public Renderer[] exRendEmblem01;
 
     [Header("Emblem Parts 2")]
     [Tooltip("Renderer which uses the second emblem")]
     public Renderer rendEmblem02;
-    [Tooltip("Additional renderers which use the second emblem")]
-    public Renderer[] exRendEmblem02;
 
     [Header("Default Colors")]
     [Tooltip("Default value for the first color")]
@@ -62,14 +56,10 @@ public class ChaClothesComponent : MonoBehaviour
     public GameObject[] objOpt01;
     [Tooltip("Renderers which can be toggled off by the second option checkbox")]
     public GameObject[] objOpt02;
-
-    [Header("Sleeve Parts")]
-    [Tooltip("Renderers for the first set of sleeves")]
-    public GameObject[] objSleeves01;
-    [Tooltip("Renderers for the second set of sleeves")]
-    public GameObject[] objSleeves02;
-    [Tooltip("Renderers for the third set of sleeves")]
-    public GameObject[] objSleeves03;
+	[Tooltip("Whether to show first optional checkbox")]
+	public bool useOpt01;
+	[Tooltip("Whether to show second optional checkbox")]
+	public bool useOpt02;
 
     [HideInInspector]
     public int initialize;
@@ -81,6 +71,13 @@ public class ChaClothesComponent : MonoBehaviour
     {
         SetMaterialsPreview();
     }
+
+	// having Start() gives Inspector enable/disable checkbox for the script
+	// needed because an exception in Awake() will disable the script
+	void Start()
+	{
+	}
+
     private void OnDestroy()
     {
         SetMaterialsOriginal();
@@ -90,42 +87,30 @@ public class ChaClothesComponent : MonoBehaviour
     {
         PreviewShaders.ReplaceShadersPreview(rendNormal01);
         PreviewShaders.ReplaceShadersPreview(rendNormal02);
-        PreviewShaders.ReplaceShadersPreview(rendNormal03);
 
         PreviewShaders.ReplaceShadersPreview(rendAlpha01);
         PreviewShaders.ReplaceShadersPreview(rendAlpha02);
 
         PreviewShaders.ReplaceShadersPreview(rendEmblem01);
-        PreviewShaders.ReplaceShadersPreview(exRendEmblem01);
         PreviewShaders.ReplaceShadersPreview(rendEmblem02);
-        PreviewShaders.ReplaceShadersPreview(exRendEmblem02);
 
         PreviewShaders.ReplaceShadersPreview(objOpt01);
         PreviewShaders.ReplaceShadersPreview(objOpt02);
-        PreviewShaders.ReplaceShadersPreview(objSleeves01);
-        PreviewShaders.ReplaceShadersPreview(objSleeves02);
-        PreviewShaders.ReplaceShadersPreview(objSleeves03);
     }
 
     public void SetMaterialsOriginal()
     {
         PreviewShaders.ReplaceShadersOriginal(rendNormal01);
         PreviewShaders.ReplaceShadersOriginal(rendNormal02);
-        PreviewShaders.ReplaceShadersOriginal(rendNormal03);
 
         PreviewShaders.ReplaceShadersOriginal(rendAlpha01);
         PreviewShaders.ReplaceShadersOriginal(rendAlpha02);
 
         PreviewShaders.ReplaceShadersOriginal(rendEmblem01);
-        PreviewShaders.ReplaceShadersOriginal(exRendEmblem01);
         PreviewShaders.ReplaceShadersOriginal(rendEmblem02);
-        PreviewShaders.ReplaceShadersOriginal(exRendEmblem02);
 
         PreviewShaders.ReplaceShadersOriginal(objOpt01);
         PreviewShaders.ReplaceShadersOriginal(objOpt02);
-        PreviewShaders.ReplaceShadersOriginal(objSleeves01);
-        PreviewShaders.ReplaceShadersOriginal(objSleeves02);
-        PreviewShaders.ReplaceShadersOriginal(objSleeves03);
     }
 #endif
 }
